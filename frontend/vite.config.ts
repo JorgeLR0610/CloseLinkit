e6845b6df6e3ffb6336c904ff360302157f31e9d
JorgeLR0610 <jorgelr061005@gmail.com>
2026-08-03 19:59:19 -0600

    feat(docker): add API and web client containers
    - Add .dockerignore and Dockerfile for frontend development.
    - Update vite.config.ts to enable HMR and access out of the container itself.
    - Add .dockerignore and Dockerfile for API setup.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: '0.0.0.0',
+      clientPort: 5173
+    },
+    watch: {
+      usePolling: true
+    }
+  },
+});   
