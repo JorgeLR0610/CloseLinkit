9746e6baee268524a80af7084aa076cc18bbe32c
JorgeLR0610 <jorgelr061005@gmail.com>
2026-09-07 18:08:40 -0600

    feat: add auth buttons
    - Add login and signup buttons on header
    - Add react-router as dependency

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -8,6 +8,14 @@ export default function Header() {
         <img src={logo} alt="CloseLinkit" className="header-logo" />
         <span className="logo-text">CloseLinkit</span>
       </div>
+      <div className="header-actions">
+        <button type="button" className="btn login-btn">
+          Log in
+        </button>
+        <button type="button" className="btn signup-btn">
+          Sign Up
+        </button>
+      </div>
     </header>
   );
 }
