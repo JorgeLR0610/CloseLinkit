f6deb9041d557e80e9387206693e3aca85ed13ed
JorgeLR0610 <jorgelr061005@gmail.com>
2026-09-12 12:18:27 -0600

    fix(db): use gen_random_uuid() for refresh token ID generation

--- server/db/queries/refresh_tokens.sql
+++ server/db/queries/refresh_tokens.sql
@@ -1,6 +1,6 @@
 -- name: CreateRefreshToken :one
-INSERT INTO refresh_tokens (id, user_id, token_hash, expires_at)
-VALUES ($1, $2, $3, $4)
+INSERT INTO refresh_tokens (user_id, token_hash, expires_at)
+VALUES ($1, $2, $3)
 RETURNING *;
 
 -- name: GetRefreshTokenByHash :one
